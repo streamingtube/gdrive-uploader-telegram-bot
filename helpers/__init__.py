1e8c33e7490e1c7f835b67b45d5c8150e6abbfba
streamingtube <82635690+streamingtube@users.noreply.github.com>
2021-04-17 11:05:18 +0530

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -5,7 +5,7 @@ from config import Config
 
 
 def start() -> scoped_session:
-    engine = create_engine(Config.DATABASE_URL)
+    engine = create_engine(Config.postgres://zqpdoezkhnmmou:46dc728a66122a24cefa29bcaebd629bd8eefbe7bf88438633b5901cdcaf7bea@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d4tbnimsjio5kf)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
