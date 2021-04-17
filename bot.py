3e07f9aa65f1570b8b222011e42a14cbf24f83a3
streamingtube <82635690+streamingtube@users.noreply.github.com>
2021-04-17 11:03:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ plugins = dict(root="plugins")
 
 
 app = Client('GDrive-Bot',
-        bot_token = Config.BOT_TOKEN,
-        api_id = Config.APP_ID,
-        api_hash = Config.API_HASH,
+        bot_token = Config.1779555807:AAH-Lb92tr99Ctbr7s_hNciBL9nM5GvX4c8,
+        api_id = Config.5952297,
+        api_hash = Config.f091a877e6760e4e51e0989531e3d430,
         plugins = plugins
       )
 
